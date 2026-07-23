27edb0b137a02039fdb1efe07c17cd705b0c23ce
oliveseeds-oss <oliveseeds.oss@gmail.com>
2026-07-23 20:42:10 +0530

    Update api.js

--- admin-panel/src/api.js
+++ admin-panel/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "http://200.141.2.131:5000/api" });
 
 // Attach token to every request
 API.interceptors.request.use((config) => {
