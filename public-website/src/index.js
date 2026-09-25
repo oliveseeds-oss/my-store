65a3e2db1b11a70e779d01fd2e021a28a465bed7
oliveseeds-oss <oliveseeds.oss@gmail.com>
2026-09-25 12:45:21 +0530

    feat: add Google fonts preconnect and premium-override.css visual stylesheet

--- public-website/src/index.js
+++ public-website/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './premium-override.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
